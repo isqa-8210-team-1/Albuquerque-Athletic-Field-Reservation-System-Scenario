57edf224e29797f5ac41d640bf26a1ccfd310b15
swetha <sbyluppala@unomaha.edu>
2019-04-03 13:43:25 -0500

    files

--- AlbuquerqueAFR/settings.py
+++ AlbuquerqueAFR/settings.py
@@ -181,13 +181,15 @@ try:
 except ImportError:
     pass
 """
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+DATABASES['default'] = dj_database_url.config()
+
 try:
     from local_settings import *
 except ImportError:
     # Update database configuration with $DATABASE_URL.
-    db_from_env = dj_database_url.config(conn_max_age=500)
-    DATABASES['default'].update(db_from_env)
-    DATABASES['default'] = dj_database_url.config()
+
     pass
 
 
