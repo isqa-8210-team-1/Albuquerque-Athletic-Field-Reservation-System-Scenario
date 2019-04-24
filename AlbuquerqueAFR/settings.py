400dd9a4662c42a1defcbadf25fe1c38e0625117
swetha <sbyluppala@unomaha.edu>
2019-04-24 16:24:34 -0500

    s3

--- AlbuquerqueAFR/settings.py
+++ AlbuquerqueAFR/settings.py
@@ -99,7 +99,7 @@ DATABASES = {
 }
 
 try:
-    from local_settings import *
+    from.local_settings import *
 except ImportError:
     # Update database configuration with $DATABASE_URL.
     db_from_env = dj_database_url.config(conn_max_age=500)
